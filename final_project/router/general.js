33877e0cec723ba2d7c8f2f070aa3a75fefeb553
PedriniRC <98079912+PedriniRC@users.noreply.github.com>
2023-04-14 18:13:29 +0900

    update general.js
    update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username && password){
+        if(!isValid(username)){
+            users.push({"username":username, "password":password});
+            return res.status(200).json({message:"User succefullly register. Now you can login"});
+        } else {
+            return res.status(404).json({message:"User already exists!"});
+        }
+    }
+  return res.status(404).json({message: "Unable to register user "});
 });
 
 // Get the book list available in the shop
@@ -18,7 +28,7 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn
+  const isbn = req.params.isbn;
   const book = books[isbn];
   if (book) {
     res.send(book);
@@ -66,8 +76,18 @@ public_users.get('/title/:title', function(req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+  
+    const book = books.find(b => b.isbn === isbn);
+  
+    if (book) {
+        // If the book is found, send its reviews
+        const reviews = book.reviews;
+        res.send(reviews);
+      } else {
+        // If the book is not found, send an error message
+        return res.status(404).json({message: "Book not found"});
+      }
 });
 
 module.exports.general = public_users;
